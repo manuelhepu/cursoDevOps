d00c17e1713769767447dee1809e340f757fa11a
manuel <manuelhepu@gmail.com>
2021-01-24 13:08:20 +0100

    Docker nginx y php

---
+++ dockerNginxPHP/src/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "MANUEL HERRERA PULIDO --CURSO DevOps";
+// Muestra toda la información, por defecto INFO_ALL
+phpinfo();
+
+// Muestra solamente la información de los módulos.
+// phpinfo(8) hace exactamente lo mismo.
+phpinfo(INFO_MODULES);
+
+?>
